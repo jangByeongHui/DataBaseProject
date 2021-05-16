73a0a1862c9282f9f9b3771a2b3c44644f6b8f7d
-m <-m>
2021-05-16 21:59:05 +0900

    네이버 가격비교 구현

---
+++ src/MealKit.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class MealKit {
+	public static void main(String[] args) throws SQLException
+	 {
+		String[] Ranks={"네이버랭킹","낮은가격순","높은가격순","등록일순","리뷰많은순"};
+		try {
+				WebResult ingredient=Search("계란",0);
+				System.out.print("재료명 : "+ ingredient.iname+"\n가격 : "+ingredient.price+"원 \n비교기준 : "+Ranks[ingredient.rank]+"\n상품 페이지 : "+ingredient.URL);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+				
+
+	 }
+	
+	
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//		함수 구현 하는 부분
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// name: 검색 대상 ,rank : 비교기준
+	//rank 0: 네이버 랭킹순 / 1: 낮은 가격순 / 2:높은 가격순 / 3:등록일순 / 4 :리뷰 많은 순
+	public static WebResult Search(String name,int rank) throws IOException {
+		
+		//rank 0: 네이버 랭킹순 / 1: 낮은 가격순 / 2:높은 가격순 / 3:등록일순 / 4 :리뷰 많은 순
+		String URL=""; // 검색 페이지
+		String PURL=""; //상품 페이지
+		String price=""; // 상품 가격
+		String pname=""; // 상품 명칭
+		if(rank==0)
+		{
+			//네이버 랭킹 첫번째
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHATC&catId=50000006&query="+name;
+			Document doc = Jsoup.connect(URL).get();
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
+			price = contents.text();
+			pname = product.text();
+			PURL=product.attr("href");
+			
+		}
+		else if(rank==1)
+		{
+			//최소 가격 0 낮은 가격순 첫번째
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&sort=price_asc&query="+name;
+			Document doc = Jsoup.connect(URL).get();
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
+			price = contents.text();
+			pname = product.text();
+		}
+		else if(rank==2)
+		{
+			//유의미한 결과를 위해 최고가격 100000원으로 설정 높은 가격순 첫번째
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&maxPrice=100000&minPrice=0&sort=price_dsc&query="+name;
+			Document doc = Jsoup.connect(URL).get();
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
+			price = contents.text();
+			pname = product.text();
+			PURL=product.attr("href");
+			
+			
+		}
+		else if(rank==3)
+		{
+			//등록일 순 첫번째 상품
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&sort=date&query="+name;
+			Document doc = Jsoup.connect(URL).get();
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span.price_num__2WUXn");
+			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
+			price = contents.text();
+			pname = product.text();
+			PURL=product.attr("href");
+			
+			
+		}
+		else if(rank==4)
+		{
+			//리뷰 많은 순 첫번째 상품
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&sort=review&query="+name;
+			Document doc = Jsoup.connect(URL).get();
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
+			price = contents.text();
+			pname = product.text();
+			PURL=product.attr("href");
+			
+			
+		}
+		else {
+			System.out.println("잘못된 입력입니다.");
+		}
+		price=price.replace("원","");
+		price=price.replace(",","");
+		int INT_PRICE=Integer.parseInt(price);
+		
+		WebResult result = new WebResult(name,PURL,INT_PRICE,rank);
+		return result;
+	}
+}
---
+++ src/WebResult.java
@@ -0,0 +1,17 @@
+
+public class WebResult {
+	
+	int price;
+	int rank;
+	String iname;
+	String URL;
+	public WebResult(String Iname,String site,int Price,int Rank)
+	{
+		this.iname=Iname;
+		this.price=Price;
+		this.rank=Rank;
+		this.URL=site;
+	}
+	
+
+}
