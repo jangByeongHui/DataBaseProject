cb71b236aa3bd5ac4e0ec0f84cc242fbe61a9999
-m <-m>
2021-05-16 23:50:58 +0900

    소스코드 변경

--- src/MealKit.java
+++ src/MealKit.java
@@ -15,8 +15,17 @@ public class MealKit {
 	 {
 		String[] Ranks={"네이버랭킹","낮은가격순","높은가격순","등록일순","리뷰많은순"};
 		try {
-				WebResult ingredient=Search("계란",0);
-				System.out.print("재료명 : "+ ingredient.iname+"\n가격 : "+ingredient.price+"원 \n비교기준 : "+Ranks[ingredient.rank]+"\n상품 페이지 : "+ingredient.URL);
+				while(true){
+				Scanner scan=new Scanner(System.in);
+				System.out.print("검색할 재료를 입력하세요:");
+				String food=scan.nextLine();
+				System.out.print("비교기준을 입력하세요 1.네이버랭킹 2. 낮은 가격순 3. 높은 가격순 4. 등록일 순 5. 리뷰많은 순:");
+				int rank=scan.nextInt();
+				
+				WebResult ingredient=Search(food,rank-1);
+				System.out.print("재료명 : "+ ingredient.iname+"\n가격 : "+ingredient.price+"원 \n비교기준 : "+Ranks[ingredient.rank]+"\n상품 페이지 : "+ingredient.URL+"\n");
+		
+				}
 		}
 		catch(IOException e)
 		{
@@ -31,7 +40,7 @@ public class MealKit {
 //		함수 구현 하는 부분
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// name: 검색 대상 ,rank : 비교기준
+	// name: 검색 대상 ,rank : 비교기준, 장보기 카테고리에서 선택
 	//rank 0: 네이버 랭킹순 / 1: 낮은 가격순 / 2:높은 가격순 / 3:등록일순 / 4 :리뷰 많은 순
 	public static WebResult Search(String name,int rank) throws IOException {
 		
@@ -43,9 +52,9 @@ public class MealKit {
 		if(rank==0)
 		{
 			//네이버 랭킹 첫번째
-			URL="https://search.shopping.naver.com/search/all?frm=NVSHATC&catId=50000006&query="+name;
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHATC&productSet=grocery&query="+name;
 			Document doc = Jsoup.connect(URL).get();
-			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span.basicList_num__1yXM9");
 			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
 			price = contents.text();
 			pname = product.text();
@@ -55,19 +64,20 @@ public class MealKit {
 		else if(rank==1)
 		{
 			//최소 가격 0 낮은 가격순 첫번째
-			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&sort=price_asc&query="+name;
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&productSet=grocery&sort=price_asc&query="+name;
 			Document doc = Jsoup.connect(URL).get();
-			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span.basicList_num__1yXM9");
 			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
 			price = contents.text();
 			pname = product.text();
+			PURL=product.attr("href");
 		}
 		else if(rank==2)
 		{
 			//유의미한 결과를 위해 최고가격 100000원으로 설정 높은 가격순 첫번째
-			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&maxPrice=100000&minPrice=0&sort=price_dsc&query="+name;
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&productSet=grocery&maxPrice=100000&minPrice=0&sort=price_dsc&query="+name;
 			Document doc = Jsoup.connect(URL).get();
-			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span.basicList_num__1yXM9");
 			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
 			price = contents.text();
 			pname = product.text();
@@ -78,9 +88,9 @@ public class MealKit {
 		else if(rank==3)
 		{
 			//등록일 순 첫번째 상품
-			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&sort=date&query="+name;
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&productSet=grocery&sort=date&query="+name;
 			Document doc = Jsoup.connect(URL).get();
-			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span.price_num__2WUXn");
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span.basicList_num__1yXM9");
 			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
 			price = contents.text();
 			pname = product.text();
@@ -91,9 +101,9 @@ public class MealKit {
 		else if(rank==4)
 		{
 			//리뷰 많은 순 첫번째 상품
-			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&catId=50000006&sort=review&query="+name;
+			URL="https://search.shopping.naver.com/search/all?frm=NVSHPRC&productSet=grocery&sort=review&query="+name;
 			Document doc = Jsoup.connect(URL).get();
-			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span > span");
+			Elements contents = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_price_area__1UXXR > strong > span.basicList_num__1yXM9");
 			Elements product = doc.select("#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div:nth-child(1) > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a");
 			price = contents.text();
 			pname = product.text();
